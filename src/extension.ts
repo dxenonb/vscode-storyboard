44d171d4131b18fb62c0e2ef600c9afdf9ac90e4
dxenonb <drdxbme@gmail.com>
2020-02-08 16:28:48 -0500

    Play with webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+interface BoardViewResources {
+	script: vscode.Uri,
+}
+
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.sequenceGraph.openBoard', () => {
+		const extensionPath = context.extensionPath;
+		const panel = vscode.window.createWebviewPanel(
+			'sequencegraph',
+			'example (Seq)',
+			vscode.ViewColumn.Active,
+			{
+				enableScripts: true,
+				localResourceRoots: [vscode.Uri.file(path.join(extensionPath, 'media'))],
+			},
+		);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sequencegraph" is now active!');
+		const paths = {
+			script: panel.webview.asWebviewUri(vscode.Uri.file(
+				path.join(extensionPath, 'media', 'main.js'),
+			)),
+		};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		panel.webview.html = getWebviewContent(panel.webview, paths);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		panel.onDidDispose(() => {
+			console.log('disposed!');
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(webview: vscode.Webview, paths: BoardViewResources) {
+	return `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta
+				http-equiv="Content-Security-Policy"
+				content="
+					default-src 'none';
+					script-src ${webview.cspSource};
+					style-src ${webview.cspSource};
+				"
+			/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Sequence Graph Board</title>
+		</head>
+		<body>
+			<p>It begins!</p>
+			<script src="${paths.script}">
+		</body>
+		</html>
+	`;
+}
